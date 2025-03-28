80eab93e1aa0ff41c9056b5110a4ca03e0464e03
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-28 18:02:14 -0400

    Add 4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('Amount must be a number');
+    }
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    if (typeof val !== 'object') {
+      throw TypeError('Currency must be of type Currency');
+    }
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw TypeError('Amount and conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
