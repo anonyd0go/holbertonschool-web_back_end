bb452e49646a3eee3f1a9183c8a3d01f95cd4ffa
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-02 13:22:46 -0400

    Correct 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ body: 'success', status: 200 })).catch(() => new Error('')).finally(() => {
+  return promise.then(() => ({ body: 'success', status: 200 })).catch(() => new Error('')).finally(() => {
     console.log('Got a response from the API');
   });
 }
