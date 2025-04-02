a850a7cf0cbad97ee2cdbee122ce2278b3fdb99f
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-02 14:49:37 -0400

    Correct 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,5 +3,16 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-  return Promise.allSettled(promises);
+  return Promise.allSettled(promises).then((results) => results.map((result) => {
+    const response = {
+      status: result.status,
+    };
+
+    if (result.status === 'fulfilled') {
+      response.value = result.value;
+    } else {
+      response.value = result.reason.toString();
+    }
+    return response;
+  }));
 }
