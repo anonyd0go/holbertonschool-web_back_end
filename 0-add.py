6de4abcddfe5e26ce49fcff9d544e0463738785a
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-23 10:13:49 -0400

    Add 0-add.py exercise

---
+++ 0-add.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module provides a simple function to add two floating-point numbers.
+
+Functions:
+    add(a: float, b: float) -> float:
+        Returns the sum of two floating-point numbers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
