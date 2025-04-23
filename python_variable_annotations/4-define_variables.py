75a24c987d3e4e4daaafbae4309eeae7a1f08e69
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-23 10:41:52 -0400

    Add 4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates variables with specific types.
+
+Variables:
+    a (int): An integer with a value of 1.
+    pi (float): A floating-point number representing the value of pi (3.14).
+    i_understand_annotations (bool): bool understanding of annotations (True).
+    school (str): A string with the value "Holberton".
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
