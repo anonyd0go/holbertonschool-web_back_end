d36fb5e2a894d6c05957efd9f5a6a67bdd69fb5f
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-23 12:27:34 -0400

    Correct 2-floor.py pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module provides a function to compute the floor of a floating-point number.
+This module provides a function to compute the floor of a float number.
 
 Functions:
     floor(n: float) -> int:
