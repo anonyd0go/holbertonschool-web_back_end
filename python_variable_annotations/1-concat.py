ef75a3c56880cb14a868a6891cfb64f293b8181a
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-23 10:23:04 -0400

    Add 1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to concatenate two strings.
+
+Functions:
+    concat(str1: str, str2: str) -> str:
+        Concatenates two strings and returns the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated result of the two strings.
+    """
+    return str1 + str2
