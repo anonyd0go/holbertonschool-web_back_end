16a33c0560553b53620f6e67f074c87c87ee7088
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-04-29 16:58:14 -0400

    Add 1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List:
+    return [x async for x in async_generator()]
