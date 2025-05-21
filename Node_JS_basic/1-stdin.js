66d018c576853a287cc922d84e71a3ce189b5715
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-21 19:57:13 -0400

    Correct 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    displayMessage(`Your name is : ${chunk}`);
+  const inpt = process.stdin.read();
+  if (inpt !== null) {
+    process.stdout.write(`Your name is : ${inpt}`);
   }
 });
 
