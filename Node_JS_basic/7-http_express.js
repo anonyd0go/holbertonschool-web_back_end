5b3233c8228cab28d547c569cd74e933610062a5
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-23 16:37:46 -0400

    Correct 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const countStudents = require('./3-read_file_async');
 
+// Set up server
 const args = process.argv.slice(2);
 const app = express();
 const port = 1245;
 
+// Home route
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
+// Get students from database
 app.get('/students', async (req, res) => {
   const msg = 'This is the list of our students\n';
   try {
