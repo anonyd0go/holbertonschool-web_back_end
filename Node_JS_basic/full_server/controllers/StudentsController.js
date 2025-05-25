804155416ebe087a247aefae9cec1cc277f45e40
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-05-25 10:11:12 -0400

    Update StudentsController.js

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,6 +1,7 @@
 const readDatabase = require('../utils');
 
 class StudentsController {
+  // Gets all students organized by Majors
   static getAllStudents(req, res, db) {
     readDatabase(db).then((fields) => {
       const response = [];
@@ -12,26 +13,30 @@ class StudentsController {
       for (const field of Object.keys(fields)) {
         msg = `Number of students in ${field}: ${fields[field].length}.
           List: ${fields[field].join(', ')}`;
-        
-          response.push(msg);
+
+        response.push(msg);
       }
 
       res.send(200, `${response.join('\n')}`);
     }).catch(() => {
+      // Send error response if unable to load DB
       res.send(500, 'Cannot load the database');
     });
   }
 
+  // Gets students by specified major
   static getAllStudentsByMajor(req, res, db) {
+    // Verify major of request
     const { major } = req.params;
-    if (major === 'CS' || major === 'SWE'){
+    if (major === 'CS' || major === 'SWE') {
+      // Gets the list of students in the specified major
       readDatabase(db).then((fields) => {
         const stdntByMajor = fields[major];
 
         res.send(200, `List: ${stdntByMajor.join(', ')}`);
       }).catch(() => {
-        res.send(500, `Cannot load the database`)
-      })
+        res.send(500, 'Cannot load the database');
+      });
     } else {
       res.send(500, 'Major parameter must be CS or SWE');
     }
